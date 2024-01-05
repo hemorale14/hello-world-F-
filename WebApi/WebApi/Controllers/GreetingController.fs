39e209e6a443c49fc385fd79eb850b1a21965fc8
Hector Rocha Morales <hector.rocha@carvajal.com>
2024-01-04 18:10:03 -0600

    [API F#] UNIR Docker usando lenguaje asp.net core/F#

---
+++ WebApi/WebApi/Controllers/GreetingController.fs
@@ -0,0 +1,18 @@
+﻿namespace WebApi.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]")>]
+type GreetingController (logger : ILogger<GreetingController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member this.Get() =
+        let values =  [|"Hello World F#/ASP.NET Core web API!"; "Alumno: Hector Rocha Morales"; "Materia: Contenedores"; "Universidad Internacional de La Rioja en México"|]
+        ActionResult<string[]>(values)
